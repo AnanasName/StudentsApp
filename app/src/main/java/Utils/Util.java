ca749a660a105d80278a0b57fc7f4131ed9845d9
AnanasName <56173538+AnanasName@users.noreply.github.com>
2020-05-04 19:32:18 +0300

    Добавлены Utils

---
+++ app/src/main/java/Utils/Util.java
@@ -0,0 +1,14 @@
+package Utils;
+
+public class Util {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "students_database";
+    public static final String TABLE_NAME = "students_info";
+
+    public static final String KEY_ID = "id";
+    public static final String KEY_FACULTY = "faculty";
+    public static final String KEY_LAST_NAME = "last_name";
+    public static final String KEY_FIRST_NAME = "first_name";
+    public static final String KEY_AVERAGE_SCORE = "average_score";
+    public static final String KEY_IMAGE = "image_data";
+}
